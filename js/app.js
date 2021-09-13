2e40009acf9dc377d2ae8de12af260f023190d1a
nhmunna <munna1720.nhm@gmail.com>
2021-09-13 19:16:56 +0600

    finalized with solve mirror problem

--- js/app.js
+++ js/app.js
@@ -14,20 +14,21 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div onclick="loadSingleProduct(${product.id})" class="single-product">
+    div.innerHTML = `<div class="single-product">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <p>rating:${product.rating.rate} out of 5(${product.rating.count})</p>
+      <p class="text-warning">rating: ${product.rating.rate} out of 5(${product.rating.count})</p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="loadSingleProduct(${product.id})" id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+//count products
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -104,7 +105,7 @@ const showSingleProductDetails = product => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <p>rating:${product.rating.rate} out of 5(${product.rating.count})</p>
+      <p class="text-warning">rating: ${product.rating.rate} out of 5(${product.rating.count})</p>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
